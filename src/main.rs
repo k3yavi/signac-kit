bca99fe2e4e153c85cc77baf35feb43c62eab00a
Avi Srivastava <asrivastava@cs.stonybrook.edu>
2020-06-05 15:38:04 -0400

    some progress

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,107 @@
-fn main() {
-    println!("Hello, world!");
+extern crate csv;
+extern crate clap;
+extern crate flate2;
+extern crate pretty_env_logger;
+
+#[macro_use]
+extern crate log;
+
+use std::fs::File;
+use std::path::Path;
+use std::error::Error;
+use std::io::BufReader;
+use std::collections::HashMap;
+use hashers::fnv::FNV1aHasher64;
+use std::hash::BuildHasherDefault;
+
+use flate2::read::MultiGzDecoder;
+use clap::{App, Arg, ArgMatches, SubCommand};
+
+fn get_map() -> HashMap<String, usize, BuildHasherDefault<FNV1aHasher64>> {
+    HashMap::default()
+}
+
+fn group_command(sub_m: &ArgMatches) -> Result<(), Box<dyn Error>> {
+    let ifile_path = Path::new(sub_m.value_of("input")
+                       .expect("can't find the input file"))
+        .canonicalize()
+        .expect("can't find absolute path of input file");
+
+    let output_file_path = sub_m.value_of("output")
+        .unwrap();
+
+    let mut column_index = match sub_m.value_of("column") {
+        Some(x) => x.parse::<usize>().expect("can't parse the column index"),
+        None => panic!("Can't find what column index to use."),
+    };
+
+    info!("Using input file {:?} and extracting {:?} column", ifile_path, column_index);
+    column_index -= 1;
+
+    let mut group_hash = get_map();
+    let file = File::open(&ifile_path).expect("can't read file");
+    let file_reader = MultiGzDecoder::new(file);
+    let file_handle = BufReader::new(file_reader);
+
+    let mut rdr = csv::ReaderBuilder::new()
+        .delimiter(b'\t')
+        .has_headers(false)
+        .from_reader(file_handle);
+
+    for result in rdr.records() {
+        let record = result?;
+        let cid = record.get(column_index).expect("can't extract the column");
+        *group_hash.entry(cid.to_string()).or_insert(0) += 1;
+    }
+
+    info!("All Done!");
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let matches = App::new("signac-kit")
+        .version("0.1.0")
+        .author("Avi Srivastava & Tim Stuart")
+        .about("A set of fast helper functions for signac R package.")
+        .subcommand(
+            SubCommand::with_name("group")
+                .about("A grouping command, analogous to group by command in R.")
+                .arg(
+                    Arg::with_name("input")
+                        .long("input")
+                        .short("i")
+                        .takes_value(true)
+                        .required(true)
+                        .help("Input fragment.gz file"),
+                )
+                .arg(
+                    Arg::with_name("column")
+                        .long("column")
+                        .short("c")
+                        .takes_value(true)
+                        .default_value("4")
+                        .help("Column Index (1-indexing) to use for grouping."),
+                )
+                .arg(
+                    Arg::with_name("output")
+                        .long("output")
+                        .short("o")
+                        .takes_value(true)
+                        .required(true)
+                        .help("Path to the output file"),
+                )
+        )
+        .get_matches();
+
+    pretty_env_logger::init_timed();
+
+    match matches.subcommand_matches("group") {
+        Some(sub_m) =>  {
+            let ret = group_command(&sub_m);
+            return ret;
+        }
+        None => ()
+    };
+
+    Ok(())
 }
