0e2debc621da89c9fe295051b6aef2ee5d125960
Avi Srivastava <asrivastava@cs.stonybrook.edu>
2020-06-12 22:18:40 -0400

    super slow workign version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate bio;
 extern crate clap;
 extern crate csv;
 extern crate flate2;
@@ -6,66 +7,11 @@ extern crate pretty_env_logger;
 #[macro_use]
 extern crate log;
 
-use std::collections::HashMap;
+use clap::{App, Arg, SubCommand};
 use std::error::Error;
-use std::fs::File;
-use std::io;
-use std::io::BufReader;
-use std::io::Write;
-use std::path::Path;
 
-use clap::{App, Arg, ArgMatches, SubCommand};
-use flate2::read::MultiGzDecoder;
-
-fn group_command(sub_m: &ArgMatches) -> Result<(), Box<dyn Error>> {
-    let ifile_path = Path::new(sub_m.value_of("input").expect("can't find the input file"))
-        .canonicalize()
-        .expect("can't find absolute path of input file");
-
-    let ofile_path = sub_m.value_of("output").unwrap();
-
-    let mut column_index = match sub_m.value_of("column") {
-        Some(x) => x.parse::<usize>().expect("can't parse the column index"),
-        None => panic!("Can't find what column index to use."),
-    };
-
-    info!(
-        "Using input file {:?} and extracting {:?} column",
-        ifile_path, column_index
-    );
-    column_index -= 1;
-
-    let mut group_hash = HashMap::<String, usize>::new();
-    let file = File::open(&ifile_path).expect("can't read file");
-    let file_reader = MultiGzDecoder::new(file);
-    let file_handle = BufReader::new(file_reader);
-
-    let mut rdr = csv::ReaderBuilder::new()
-        .delimiter(b'\t')
-        .has_headers(false)
-        .from_reader(file_handle);
-
-    for (index, result) in rdr.records().enumerate() {
-        let record = result?;
-        let cid = record.get(column_index).expect("can't extract the column");
-        *group_hash.entry(cid.to_string()).or_insert(0) += 1;
-
-        if index % 1_000_000 == 0 {
-            print!("\r Done processing {:?} Million lines", index / 1_000_000);
-            io::stdout().flush().unwrap();
-        }
-    }
-    println!();
-    info!("Done Parsing the input file, writing output");
-
-    let mut ofile = File::create(&ofile_path).expect("can't create output file");
-    for (k, v) in group_hash.into_iter() {
-        writeln!(&mut ofile, "{}\t{}", k, v)?;
-    }
-
-    info!("All Done!");
-    Ok(())
-}
+mod extract;
+mod group;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let matches = App::new("signac-kit")
@@ -100,17 +46,52 @@ fn main() -> Result<(), Box<dyn Error>> {
                         .help("Path to the output file"),
                 ),
         )
+        .subcommand(
+            SubCommand::with_name("extract")
+                .about("A barcode extraction command, attaches CB to the header of the reads.")
+                .arg(
+                    Arg::with_name("1")
+                        .long("1")
+                        .short("1")
+                        .takes_value(true)
+                        .required(true)
+                        .help("R1_001.fastq.gz file, typically left end of the read"),
+                )
+                .arg(
+                    Arg::with_name("2")
+                        .long("2")
+                        .short("2")
+                        .takes_value(true)
+                        .required(true)
+                        .help("R2_001.fastq.gz file, typically barcode containing read"),
+                )
+                .arg(
+                    Arg::with_name("3")
+                        .long("3")
+                        .short("3")
+                        .takes_value(true)
+                        .required(true)
+                        .help("R3_001.fastq.gz file, typically right end of the read"),
+                ),
+        )
         .get_matches();
 
     pretty_env_logger::init_timed();
 
     match matches.subcommand_matches("group") {
         Some(sub_m) => {
-            let ret = group_command(&sub_m);
+            let ret = group::group_command(&sub_m);
             return ret;
         }
         None => (),
     };
 
+    match matches.subcommand_matches("extract") {
+        Some(sub_m) => {
+            let ret = extract::extract_command(&sub_m);
+            return ret;
+        }
+        None => (),
+    };
     Ok(())
 }
